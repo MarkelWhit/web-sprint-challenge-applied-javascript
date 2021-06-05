204c666a6c989797d0da96d47e4ce676355e5796
MarkelWhit <litslayer@gmail.com>
2021-06-04 19:28:02 -0500

    working with card

--- src/components/card.js
+++ src/components/card.js
@@ -2,8 +2,8 @@
 import axios from 'axios'
 
 
- const entryPoint = document.querySelector('.cards-container')
- const Card = (article) => {
+const entryPoint = document.querySelector('.cards-container')
+const Card = ({ headline, authorPhoto, author, }) => {
   // TASK 5
   // ---------------------
   // Implement this function, which should return the markup you see below.
@@ -11,29 +11,29 @@ import axios from 'axios'
   // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
   // The text inside elements will be set using their `textContent` property (NOT `innerText`).
   // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
- const cards = document.createElement('div')
- const head = document.createElement('div')
- const auth = document.createElement('div')
- const imageCon = document.createElement('div')
- const image = document.createElement('img')
- const artist = document.createElement('span')
- 
- cards.classList.add('card')
- head.classList.add('headline')
- auth.classList.add('author')
- imageCon.classList.add('img-container')
- 
- head.textContent = 
- image.src = `${authorPhoto}`
- artist.textContent=`By ${author}`
+  const cards = document.createElement('div')
+  const head = document.createElement('div')
+  const auth = document.createElement('div')
+  const imageCon = document.createElement('div')
+  const image = document.createElement('img')
+  const artist = document.createElement('span')
 
- cards.appendChild(head)
- head.appendChild(auth)
- auth.appendChild(imageCon)
-imageCon.appendChild(image) 
-imageCon.appendChild(artist)
+  cards.classList.add('card')
+  head.classList.add('headline')
+  auth.classList.add('author')
+  imageCon.classList.add('img-container')
 
-// <div class="card">
+  head.textContent = `${headline}`
+  image.src = `${authorPhoto}`
+  artist.textContent = `By ${author}`
+
+  cards.appendChild(head)
+  head.appendChild(auth)
+  auth.appendChild(imageCon)
+  imageCon.appendChild(image)
+  imageCon.appendChild(artist)
+
+  // <div class="card">
   //   <div class="headline">{ headline }</div>
   //   <div class="author">
   //     <div class="img-container">
@@ -43,22 +43,27 @@ imageCon.appendChild(artist)
   //   </div>
   // </div>
   //
-return cards
+  return cards
 }
-entryPoint.appendChild(Card())
-const cardAppender = (selector) => {
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `https://lambda-times-api.herokuapp.com/articles`
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
-  axios.get(`https://lambda-times-api.herokuapp.com/articles`)
+entryPoint.appendChild(Card({}))
+const cardAppender = () => {
+
+// TASK 6
+// ---------------------
+// Implement this function that takes a css selector as its only argument.
+// It should obtain articles from this endpoint: `https://lambda-times-api.herokuapp.com/articles`
+// However, the articles do not come organized in a single, neat array. Inspect the response closely!
+// Create a card from each and every article object in the response, using the Card component.
+// Append each card to the element in the DOM that matches the selector passed to the function.
+//
+
+//}
+axios.get('https://lambda-times-api.herokuapp.com/articles')
   .then(res => {
     console.log(res.data)
   })
+  .catch(err => {
+    console.log(err)
+  })
 }
-
 export { Card, cardAppender }
